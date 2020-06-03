b677a36c9693d0f3bf3240537f27c5669586d1d3
Pritam Singh Negi <31372155+preetamnegi7@users.noreply.github.com>
2020-06-03 10:18:42 +0200

    Hindi

--- src/components/map.js
+++ src/components/map.js
@@ -17,6 +17,7 @@ export default function Map() {
         mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
       >
         <h2>Welcome to uttarakhand</h2>
+        <h2>देवभूमि उत्तराखंड मा आपक स्वागत च</h2>
       </ReactMapGL>
     </div>
   );
