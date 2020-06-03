24601b45cfcc26466ace36b3a1a2f3101e1f665d
Pritam Singh Negi <31372155+preetamnegi7@users.noreply.github.com>
2020-06-03 09:46:14 +0200

    -Map

---
+++ src/components/map.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import ReactMapGL from "react-map-gl";
+
+export default function Map() {
+  const [viewport, setViewport] = useState({
+    latitude: 30.0668,
+    longitude: 79.0193,
+    width: "100vw",
+    height: "100vh",
+    zoom: 8,
+  });
+
+  return (
+    <div>
+      <ReactMapGL
+        {...viewport}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      >
+        <h2>Welcome to uttarakhand</h2>
+      </ReactMapGL>
+    </div>
+  );
+}
