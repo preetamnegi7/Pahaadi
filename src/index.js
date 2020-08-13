d100d1301628840acebf7877fdd1ae9f95367983
Pritam Singh Negi <31372155+preetamnegi7@users.noreply.github.com>
2020-08-13 02:35:43 +0200

    Removed App.js access from index temp

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
+
 import Map from "./components/map";
 import * as serviceWorker from "./serviceWorker";
 
