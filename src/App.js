48b878fb1631e57683799a7ae217f49048fa9b78
Pritam Singh Negi <31372155+preetamnegi7@users.noreply.github.com>
2020-08-16 23:58:46 +0200

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,77 +1,25 @@
-import React, { useState, useEffect } from "react";
-import ReactMapGL, { Marker, Popup, GeolocateControl } from "react-map-gl";
-import * as gymData from "./Data/data.json";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import { CssBaseline } from "@material-ui/core";
+import PrimarySearchAppBar from "./components/Navbar/Header";
+import Home from "./components/home";
+import Error from "./components/Error";
 import "./App.css";
-
-export default function App() {
-  const [viewport, setViewport] = useState({
-    latitude: 52.2297,
-    longitude: 21.0122,
-    zoom: 14,
-    width: "100vw",
-    height: "100vh",
-  });
-
-  const [selectedGym, setSelectedGym] = useState(null);
-
-  useEffect(() => {
-    const listener = (e) => {
-      if (e.key === "Escape") {
-        setSelectedGym(null);
-      }
-    };
-    window.addEventListener("keydown", listener);
-    return () => {
-      window.removeEventListener("keydown", listener);
-    };
-  }, []);
+import Map from "./components/Map";
+import LabelBottomNavigation from "./components/Navbar/Footer";
+const App = () => {
   return (
-    <div>
-      <ReactMapGL
-        {...viewport}
-        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
-        mapStyle="mapbox://styles/preetamnegi7/ckcpzafdy0y5g1il910ddvc5w"
-        onViewportChange={(viewport) => {
-          setViewport(viewport);
-        }}
-      >
-        {gymData.features.map((gym) => (
-          <Marker
-            key={gym.properties.GYM_ID}
-            latitude={gym.geometry.coordinates[0]}
-            longitude={gym.geometry.coordinates[1]}
-          >
-            <button
-              className="gym-logo"
-              onClick={(e) => {
-                e.preventDefault();
-                setSelectedGym(gym);
-              }}
-            >
-              <img src="gym.svg" alt="Gym Icon" />
-            </button>
-          </Marker>
-        ))}
-        {selectedGym ? (
-          <Popup
-            latitude={selectedGym.geometry.coordinates[0]}
-            longitude={selectedGym.geometry.coordinates[1]}
-            onClose={() => {
-              setSelectedGym(null);
-            }}
-          >
-            <div>
-              <h2>{selectedGym.properties.NAME}</h2>
-              <p>{selectedGym.properties.ADDRESS}</p>
-            </div>
-          </Popup>
-        ) : null}
-
-        <GeolocateControl
-          positionOptions={{ enableHighAccuracy: true }}
-          trackUserLocation={true}
-        />
-      </ReactMapGL>
-    </div>
+    <>
+      <CssBaseline />
+      <PrimarySearchAppBar />
+      <Map />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route component={Error} />
+      </Switch>
+      <LabelBottomNavigation />
+    </>
   );
-}
+};
+
+export default App;
